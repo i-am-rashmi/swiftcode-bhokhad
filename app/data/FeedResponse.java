fa8e450963b3d775037f10d852f18eb93a814797
rashmi <rashminiranjan96@gmail.com>
2017-10-06 10:48:57 +0000

    Fixing Commit Changes

---
+++ app/data/FeedResponse.java
@@ -0,0 +1,12 @@
+package data;
+
+public class FeedResponse {
+    public String keyword;
+d
+    public String intent;
+
+
+
+
+
+}
---
+++ app/data/Message.java
@@ -0,0 +1,9 @@
+package data;
+
+public class Message {
+    public String text;
+    public String timestamp;
+    public enum Sender {BOT,USER};
+    public Sender sender;
+
+}
