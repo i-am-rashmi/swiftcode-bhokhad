60b4a9d6015b28c4d0da811bbb83cfd528abd5cb
Rashmi <rashminiranjan96@gmail.com>
2017-10-08 09:47:33 +0000

    Now evrything works need to do a bit of UI

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,7 +2,7 @@ var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
     $scope.messages = [] ;
-    var  exampleSocket =  new  WebSocket("wss://swiftcode-ws-chat.herokuapp.com/chatSocket");
+    var  exampleSocket =  new  WebSocket('ws://localhost:9000/chatSocket');
         exampleSocket.onmessage  =   function  (event) {
             var jsonData = JSON.parse(event.data);
             jsonData.time = new Date()
