295d6f901d97ad9e3a95e06d0a0836ba4a73ab20
Rashmi <rashminiranjan96@gmail.com>
2017-10-08 10:29:39 +0000

    UI done to an extent

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,18 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('purple',{
+            'default':'900',// by default use shade 400 from the pink palette for primary intentions
+            'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+            'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+            'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+        })
+
+        .accentPalette('indigo',{
+            'default':'300'});
+});
+
 app.controller('chatController', function ($scope, $sce) {
     $scope.messages = [] ;
     var  exampleSocket =  new  WebSocket('ws://localhost:9000/chatSocket');
