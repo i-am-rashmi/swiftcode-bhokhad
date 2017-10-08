130c2ca3917fc6f09c1832f31ab7e773330a368d
Rashmi <rashminiranjan96@gmail.com>
2017-10-08 04:40:55 +0000

    changes 5.0

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,41 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+    $scope.messages = [
+    {
+
+        'sender':'USER' ,
+        'text':'hi',
+        'time':'10:01 AM'
+
+    },
+    {
+
+            'sender':'BOT',
+            'text':'What can I do for you?',
+            'time':'10:01 AM'
+
+    },
+    {
+
+                'sender':'USER',
+                'text':'News about Modi',
+                'time':'10:01 AM'
+    },
+     {
+
+                    'sender':'BOT',
+                    'text':'INVALID',
+                    'time':'10:01 AM'
+        },
+         {
+
+                        'sender':'USER',
+                        'text':'News about Rahul Gandhi',
+                        'time':'10:01 AM'
+            }
+
+    ]
+
+
 });
