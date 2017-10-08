996102b874d1909ffe4b661886573dea377ffb79
Rashmi <rashminiranjan96@gmail.com>
2017-10-08 10:41:01 +0000

    final

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,15 +2,16 @@ var app = angular.module('chatApp', ['ngMaterial']);
 
 app.config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('default')
-        .primaryPalette('purple',{
-            'default':'900',// by default use shade 400 from the pink palette for primary intentions
+        .primaryPalette('grey',{
+            'default':'400',// by default use shade 400 from the pink palette for primary intentions
             'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
             'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
             'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
         })
 
-        .accentPalette('indigo',{
-            'default':'300'});
+        .accentPalette('cyan',{
+            'default':'A400'})
+            .dark();
 });
 
 app.controller('chatController', function ($scope, $sce) {
